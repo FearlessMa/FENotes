bf148581aa7ae86cc4738802d77f2aa77adbd1e8
FearlessMa <458871138@qq.com>
2020-07-27 11:29:57 +0800

    fix: path bug

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ module.exports = {
   description: 'FENotes',
   dest: './dist',
   base: '/FENotes/',
+  // base: '/dist/',
   themeConfig: {
     editLinks: false,
     // 假如文档不是放在仓库的根目录下：
@@ -18,11 +19,11 @@ module.exports = {
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 2, // 可选的, 默认值是 1
         children: [
-          ['vue/', '目录'],
+          ['/vue/', '目录'],
           {
             title: 'vue基础',
             sidebarDepth: 2,
-            children: [['vue/vueBase/', '目录'], 'vue/vueBase/vueBase1']
+            children: [['/vue/vueBase/', '目录'], '/vue/vueBase/vueBase1']
           }
         ]
       }
