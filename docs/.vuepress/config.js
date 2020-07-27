414f19641701705b334084d5d0ea681142e62013
FearlessMa <458871138@qq.com>
2020-07-27 10:57:05 +0800

    build: 增加 basePath

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'FENotes',
   description: 'FENotes',
   dest: './dist',
-  // base: './',
+  base: '/',
   themeConfig: {
     editLinks: false,
     // 假如文档不是放在仓库的根目录下：
