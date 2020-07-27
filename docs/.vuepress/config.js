850ef6996b712634f0e4fabc4663af23e10ebb67
FearlessMa <458871138@qq.com>
2020-07-27 11:11:30 +0800

    build: test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'FENotes',
   description: 'FENotes',
   dest: './dist',
-  base: './FENotes/',
+  // base: '/',
   themeConfig: {
     editLinks: false,
     // 假如文档不是放在仓库的根目录下：
@@ -18,11 +18,11 @@ module.exports = {
         collapsable: true, // 可选的, 默认值是 true,
         sidebarDepth: 2, // 可选的, 默认值是 1
         children: [
-          ['/vue/', '目录'],
+          ['vue/', '目录'],
           {
             title: 'vue基础',
             sidebarDepth: 2,
-            children: [['/vue/vueBase/', '目录'], '/vue/vueBase/vueBase1']
+            children: [['vue/vueBase/', '目录'], 'vue/vueBase/vueBase1']
           }
         ]
       }
