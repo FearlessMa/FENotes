6d9cf6b1df3b86203255945d67276763ff52ef33
FearlessMa <458871138@qq.com>
2021-02-10 13:28:00 +0800

    feat: 增加主页

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,20 +3,28 @@ module.exports = {
   description: 'FENotes',
   dest: './dist',
   base: '/FENotes/',
-  plugins: ['@vuepress/back-to-top', '@vuepress/nprogress'],
+  plugins: ['@vuepress/back-to-top'],
   markdown: {
     lineNumbers: true // 代码块显示行号
   },
   // theme: 'antdocs',
   // base: '/dist/',
   themeConfig: {
+    home: true,
+    nav: [
+      { text: '主页', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'GitHub', link: 'https://github.com/FearlessMa/FENotes' }
+    ],
     editLinks: false,
     // 假如文档不是放在仓库的根目录下：
     // docsDir: 'docs',
     // 假如文档放在一个特定的分支下：
     docsBranch: 'master',
     // sidebar: 'auto',
+    // sidebar: 'auto',
     // lastUpdated: 'Last Updated', // string | boolean
+    displayAllHeaders: false, // 显示所有页面的标题链接    默认值：false
     smoothScroll: true,
     sidebar: [
       {
@@ -27,7 +35,7 @@ module.exports = {
           ['/js/', '目录'],
           {
             title: '日常笔记',
-            sidebarDepth: 2,
+            sidebarDepth: 0,
             children: ['/js/notes/mouseWheel']
           }
         ]
@@ -103,7 +111,26 @@ module.exports = {
           '/tools/devTools/'
         ]
       }
-    ]
+    ],
+    //Git 仓库和编辑链接
+    // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    repo: 'https://github.com/FearlessMa/FENotes',
+    // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    repoLabel: '查看源码',
+
+    // 以下为可选的编辑链接选项
+
+    // 假如你的文档仓库和项目本身不在一个仓库：
+    // docsRepo: 'vuejs/vuepress',
+    // 假如文档不是放在仓库的根目录下：
+    docsDir: 'docs',
+    // 假如文档放在一个特定的分支下：
+    docsBranch: 'master',
+    // 默认是 false, 设置为 true 来启用
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '在 GitHub 上编辑此页'
   },
   configureWebpack: {
     resolve: {
